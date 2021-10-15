21161e62ec1113ae102093866e6f9d5cbdd1ef7b
TERESIA012 <kingoriteresia@gmail.com>
2021-10-15 23:29:37 +0300

    modify models

--- iclone/models.py
+++ iclone/models.py
@@ -6,9 +6,42 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	bio = models.CharField(max_length = 300,blank = True,default = 'Awesome Bio Will Appear Here')
-	profile_pic = models.ImageField(upload_to = 'profile/', blank = True,default = '../static/images/default.png')
+	profile_pic = models.ImageField(upload_to = 'profile/', blank = True,default = '../static/assets/terry.jpeg')
 	user = models.ForeignKey(User, on_delete = models.CASCADE)
 	
 
 	def __str__(self):
 		return self.user
+
+class Image(models.Model):
+	
+	image_name = models.CharField(max_length = 60, blank = True)
+	image_caption = models.CharField(max_length = 60, blank = True)
+	created_at = models.DateTimeField(auto_now_add = True)
+	profile = models.ForeignKey(User)
+	user_profile = models.ForeignKey(Profile)
+	likes = models.ManyToManyField(User,related_name = 'likes', blank = True)
+	image = models.ImageField(upload_to = 'images/', blank = True)
+
+	@classmethod
+	def save_image(self):
+		self.save()
+
+	@classmethod
+	def delete_image(self):
+		self.delete()
+
+	@classmethod
+	def update_caption(cls,id,caption):
+		updated_caption = cls.objects.filter(pk = id).update(image_caption = caption)
+		return updated_caption	
+
+	@classmethod
+	def get_image_by_id(cls,image_id):
+		image = cls.objects.get(id = image_id)
+		return image
+	def total_likes(self):
+		self.likes.count()
+
+	def __str__(self):
+		return self.image_name
