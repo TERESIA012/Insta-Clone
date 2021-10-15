71fb2896c78ecf81a69c11407f77d1ee20b43dcd
TERESIA012 <kingoriteresia@gmail.com>
2021-10-15 23:23:30 +0300

    create class profile

--- iclone/models.py
+++ iclone/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+	bio = models.CharField(max_length = 300,blank = True,default = 'Awesome Bio Will Appear Here')
+	profile_pic = models.ImageField(upload_to = 'profile/', blank = True,default = '../static/images/default.png')
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	
+
+	def __str__(self):
+		return self.user
