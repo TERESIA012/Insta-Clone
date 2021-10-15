14458cf56943c16b09a65d2e617b45207204431e
TERESIA012 <kingoriteresia@gmail.com>
2021-10-15 23:41:47 +0300

    create function def index

--- iclone/views.py
+++ iclone/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from .forms import NewImagePost,CreateComment,UpdateProfile
+from .models import Image,Comment,Profile,User,Follow
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 # Create your views here.
+
+def index(request):
+	'''
+	Method that fetches all images from all users.
+	'''
+	images = Image.objects.all()
+	title = "Discover"
+	
+	return render(request,'index.html',{"images":images,"title":title})
