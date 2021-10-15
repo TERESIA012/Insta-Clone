d1a90d988fd9ab1e4ea18eac2e9a8cdb29b2152d
TERESIA012 <kingoriteresia@gmail.com>
2021-10-16 02:54:05 +0300

    add urls

--- iclone/models.py
+++ iclone/models.py
@@ -1,75 +1,99 @@
 from django.db import models
-import datetime as dt
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your models here.
 
-class Profile(models.Model):
-	bio = models.CharField(max_length = 300,blank = True,default = 'Awesome Bio Will Appear Here')
-	profile_pic = models.ImageField(upload_to = 'profile/', blank = True,default = '../static/assets/terry.jpeg')
-	user = models.ForeignKey(User, on_delete = models.CASCADE)
-	
-
-	def __str__(self):
-		return self.user
-
+# likes = models.ManyToManyField(User, related_name='likes')
 class Image(models.Model):
-	
-	image_name = models.CharField(max_length = 60, blank = True)
-	image_caption = models.CharField(max_length = 60, blank = True)
-	created_at = models.DateTimeField(auto_now_add = True)
-	profile = models.ForeignKey(User)
-	user_profile = models.ForeignKey(Profile)
-	likes = models.ManyToManyField(User,related_name = 'likes', blank = True)
-	image = models.ImageField(upload_to = 'images/', blank = True)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to = 'assets/', null=True, blank=True)
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=30)
 
-	@classmethod
-	def save_image(self):
-		self.save()
+    class Meta:
+        ordering = ["-pk"]
 
-	@classmethod
-	def delete_image(self):
-		self.delete()
+   
 
-	@classmethod
-	def update_caption(cls,id,caption):
-		updated_caption = cls.objects.filter(pk = id).update(image_caption = caption)
-		return updated_caption	
+    @classmethod
+    def images(cls):
+        images = cls.objects.all()
+        return images
 
-	@classmethod
-	def get_image_by_id(cls,image_id):
-		image = cls.objects.get(id = image_id)
-		return image
-	def total_likes(self):
-		self.likes.count()
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
 
-	def __str__(self):
-		return self.image_name
+    def save_image(self):
+        self.save()
 
-class Comment(models.Model):
-	comment = models.CharField(max_length = 1000)
-	created_at = models.DateTimeField(auto_now_add = True)
-	image = models.ForeignKey(Image)
-	profile = models.ForeignKey(User)
+    def delete_image(self):
+        self.delete()
 
-	def __str__(self):
-		return self.profile
+    @classmethod
+    def update_image(cls,old,new):
+        cap = Image.objects.filter(caption=old).update(caption=new)
+        return cap
 
-#Add the following field to User 
-def get_first_name(self):
-    return self.first_name
+    def __str__(self):
+        return self.name
+
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
+    photo = models.ImageField(upload_to = 'gallery/', null=True, blank=True, default='download.jpeg')
+    bio = models.CharField(max_length=300)
+    name = models.CharField(blank=True, max_length=120)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    @classmethod
+    def profile(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+    def photo_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+
+    def save_profile(self):
+        self.user
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
 
 class Follow(models.Model):
-    user_from = models.ForeignKey(User,related_name='rel_from_set')
-    user_to = models.ForeignKey(User, related_name='rel_to_set')
-  
-    def __str__(self):
-        return '{} follows {}'.format(self.user_from, self.user_to)
-    
-    
-User.add_to_class('following',
-                  models.ManyToManyField('self',
-                                         through=Follow,
-                                         related_name='followers',
-                                         symmetrical=False))    
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
 
+    def __str__(self):
+        return f'{self.follower} Follow'
+
+class Comment(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey('Profile',on_delete=models.CASCADE,related_name='comment')
+    photo = models.ForeignKey('Image',on_delete=models.CASCADE,related_name='comment')
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f'{self.user.name} Image'
