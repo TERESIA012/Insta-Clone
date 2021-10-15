c0b735d804dd76e27d4dddcf66086897ba5040e1
TERESIA012 <kingoriteresia@gmail.com>
2021-10-16 02:24:31 +0300

    add profile function

--- iclone/views.py
+++ iclone/views.py
@@ -25,3 +25,24 @@ def timeline(request):
 	follows = Follow.objects.filter(user_from = request.user.id)
 	images = Image.objects.filter(profile = request.user.following.user_to)
 	return render(request, 'accounts/timeline.html',{"images":images,"follows":follows})
+
+
+def profile(request,prof_id):
+	'''
+	Method that fetches a users profile page
+	'''
+	user=User.objects.get(pk=prof_id)
+	images = Image.objects.filter(profile = prof_id)
+	title = User.objects.get(pk = prof_id).username
+	profile = Profile.objects.filter(user = prof_id)
+
+	if Follow.objects.filter(user_from=request.user,user_to = user).exists():
+		is_follow = True
+	else:
+		is_follow = False
+
+	followers = Follow.objects.filter(user_to = user).count()
+	following = Follow.objects.filter(user_from = user).count()
+	
+
+	return render(request,'accounts/profile.html',{"images":images,"profile":profile,"title":title,"is_follow":is_follow,"followers":followers,"following":following})
