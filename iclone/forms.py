fadafff9678eb3255657f058120b64bd2342d921
TERESIA012 <kingoriteresia@gmail.com>
2021-10-16 03:04:13 +0300

    updateuserform

--- iclone/forms.py
+++ iclone/forms.py
@@ -14,3 +14,10 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ('photo','name','bio')
 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
